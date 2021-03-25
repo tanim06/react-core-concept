d5d5b81bcd9b48104ccd2328f7206cbf831278c6
tanim06 <tanimsarker06@gmail.com>
2021-03-25 14:28:01 -0600

    Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const Pname = ['Tanim', 'Vubon' , 'Anas Abdulla' , 'Tonmoy']
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>I am a react person</h1>
+     <Person name ="Tanim" famework="React JS"></Person>
+     <Person name="Masum"></Person>
+     <Person name="Vubon Paul"></Person>
+      <Person name={Pname[2]}></Person>
     </div>
   );
 }
-
+function Person(props){
+  const personStyle = {
+    border:"2px solid blue",
+    margin : "10px"
+  }
+  
+  console.log(props);
+  return (
+ 
+  <div style={personStyle}>
+      <p>Name: <b>{props.name}</b></p>
+      <h3>Hero of the year</h3>
+  </div>
+  )
+}
 export default App;
